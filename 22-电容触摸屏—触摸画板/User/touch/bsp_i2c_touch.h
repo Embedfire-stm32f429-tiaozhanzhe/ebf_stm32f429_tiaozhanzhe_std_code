281b049225e354f841cdc5662291e9f6fca10f1b
flyleaf91 <flyleaf91@163.com>
2016-06-03 18:45:58 +0800

    触摸屏增加软件IIC，更新7寸屏触摸配置参数

--- 22-电容触摸屏—触摸画板/User/touch/bsp_i2c_touch.h
+++ 22-电容触摸屏—触摸画板/User/touch/bsp_i2c_touch.h
@@ -3,6 +3,10 @@
 
 #include "stm32f4xx.h"
 
+/*使用软件IIC，把SOFT_IIC宏设置为1，硬件IIC则设置为0
+!!使用硬件IIC时非常容易出现错误，不推荐*/
+#define SOFT_IIC      1
+
 /*设定使用的电容屏IIC设备地址*/
 #define GTP_ADDRESS            0xBA
 
@@ -29,20 +33,29 @@
 /*复位引脚*/
 #define GTP_RST_GPIO_PORT                GPIOD
 #define GTP_RST_GPIO_CLK                 RCC_AHB1Periph_GPIOD
-#define GTP_RST_GPIO_PIN                 GPIO_Pin_13
+#define GTP_RST_GPIO_PIN                 GPIO_Pin_11
 /*中断引脚*/
 #define GTP_INT_GPIO_PORT                GPIOD
 #define GTP_INT_GPIO_CLK                 RCC_AHB1Periph_GPIOD
-#define GTP_INT_GPIO_PIN                 GPIO_Pin_11
+#define GTP_INT_GPIO_PIN                 GPIO_Pin_13
 #define GTP_INT_EXTI_PORTSOURCE          EXTI_PortSourceGPIOD
-#define GTP_INT_EXTI_PINSOURCE           EXTI_PinSource11
-#define GTP_INT_EXTI_LINE                EXTI_Line11
+#define GTP_INT_EXTI_PINSOURCE           EXTI_PinSource13
+#define GTP_INT_EXTI_LINE                EXTI_Line13
 #define GTP_INT_EXTI_IRQ                 EXTI15_10_IRQn
 /*中断服务函数*/
 #define GTP_IRQHandler                   EXTI15_10_IRQHandler
 
 
+//软件IIC使用的宏
+#define I2C_SCL_1()  GPIO_SetBits(GTP_I2C_SCL_GPIO_PORT, GTP_I2C_SCL_PIN)		/* SCL = 1 */
+#define I2C_SCL_0()  GPIO_ResetBits(GTP_I2C_SCL_GPIO_PORT, GTP_I2C_SCL_PIN)		/* SCL = 0 */
 
+#define I2C_SDA_1()  GPIO_SetBits(GTP_I2C_SDA_GPIO_PORT, GTP_I2C_SDA_PIN)		/* SDA = 1 */
+#define I2C_SDA_0()  GPIO_ResetBits(GTP_I2C_SDA_GPIO_PORT, GTP_I2C_SDA_PIN)		/* SDA = 0 */
+
+#define I2C_SDA_READ()  GPIO_ReadInputDataBit(GTP_I2C_SDA_GPIO_PORT, GTP_I2C_SDA_PIN)	/* 读SDA口线状态 */
+
+//函数接口
 void I2C_Touch_Init(void);
 uint32_t I2C_WriteBytes(uint8_t ClientAddr,uint8_t* pBuffer,  uint8_t NumByteToWrite);
 uint32_t I2C_ReadBytes(uint8_t ClientAddr,uint8_t* pBuffer, uint16_t NumByteToRead);
